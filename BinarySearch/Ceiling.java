9ea14174459c01cd63948d518002526a67f8f202
Pratik Bhagat <bhagatpratik.2002@gmail.com>
2021-10-11 16:50:39 +0530

    started interview questions

---
+++ BinarySearch/Ceiling.java
@@ -0,0 +1,29 @@
+package com.pratik;
+
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] array = {1,2,4,6,9};
+        int target = 3;
+        System.out.println(Ceiling(array,target));
+    }
+    static int Ceiling(int[] array,int target){
+        int start = 0;
+        int end = array.length -1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+
+            if (target == array[mid]){
+                return mid;
+            }
+            if (target > array[mid]){
+                start = mid + 1;
+            }else if (target < array[mid]){
+                end = mid - 1;
+            }else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
